6b0c2e1b972d1c989d6dd26d5ccb209b727023cc
Ono Keita <oh.kitbsk.1028@icloud.com>
2023-12-23 09:19:22 +0000

    feat: カレンダー用のコンポーネント作成

---
+++ src/components/Calendar/Month.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+import getMonthDetails from '../../configs/util';
+
+import Day from './Day';
+import classes from './Month.module.scss';
+
+import type { Days } from '../../types/days';
+import type { MonthProps } from '../../types/props';
+
+function Month(props: MonthProps) {
+  const { year, month, monthlyBalance } = props;
+  const [monthDetails, setMonthDetails] = useState<Days>([]);
+
+  useEffect(() => {
+    setMonthDetails(getMonthDetails(year, month));
+  }, [year, month]);
+
+  const renderMonthList = () => {
+    const calender: JSX.Element[] = [];
+    const tempArray: Days = [];
+    monthDetails.forEach((e1) => {
+      monthlyBalance.forEach((e2) => {
+        const tempObj = e1;
+        if (e1.date === Number(e2.date.slice(-2))) {
+          tempObj.spendingTotal = e2.spendingTotal;
+          tempObj.incomeTotal = e2.incomeTotal;
+        }
+        tempArray.push(tempObj);
+      });
+      calender.push(
+        <div key={e1.id} className={classes.container_day}>
+          <Day date={e1.date} spendingTotal={e1.spendingTotal} incomeTotal={e1.incomeTotal} />
+        </div>
+      );
+    });
+    return calender;
+  };
+
+  return <div className={classes.container_month}>{renderMonthList()}</div>;
+}
+
+export default Month;
