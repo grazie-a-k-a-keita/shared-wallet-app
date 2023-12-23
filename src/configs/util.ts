b779791d817b1f5f9df2204f40f4a1f893d92010
Ono Keita <oh.kitbsk.1028@icloud.com>
2023-12-23 09:21:48 +0000

    feat: 月の詳細情報（曜日、週など）を取得する関数作成

---
+++ src/configs/util.ts
@@ -0,0 +1,68 @@
+import dayjs from 'dayjs';
+import { v4 as uuid } from 'uuid';
+
+import type { Days } from '../types/days';
+
+function getMonthDetails(year: number, month: number) {
+  const daysArray: Days = [];
+  const startDate: dayjs.Dayjs = dayjs(`${year}-${month}-01`);
+  const endDate: dayjs.Dayjs = startDate.endOf('month');
+  let currentDate: dayjs.Dayjs = startDate;
+
+  while (currentDate.isBefore(endDate) || currentDate.isSame(endDate, 'day')) {
+    daysArray.push({
+      id: uuid(),
+      date: currentDate.date(),
+      day: currentDate.format('ddd'),
+      thisMonth: true,
+    });
+    currentDate = currentDate.add(1, 'day');
+  }
+
+  // 日曜始まりになるように配列の数を合わせる
+  if (daysArray[0].day !== 'Sun') {
+    let addCount = 0;
+    switch (daysArray[0].day) {
+      case 'Sat':
+        addCount = 6;
+        break;
+      case 'Fri':
+        addCount = 5;
+        break;
+      case 'Thu':
+        addCount = 4;
+        break;
+      case 'Wed':
+        addCount = 3;
+        break;
+      case 'Tue':
+        addCount = 2;
+        break;
+      case 'Mon':
+        addCount = 1;
+        break;
+      default:
+        break;
+    }
+
+    for (let i = 0; i < addCount; i += 1) {
+      daysArray.unshift({
+        id: uuid(),
+        thisMonth: false,
+      });
+    }
+  }
+
+  // daysArrayの配列が42コになるようにpush()する
+  if (daysArray.length !== 42) {
+    for (let i = daysArray.length; i < 42; i += 1) {
+      daysArray.push({
+        id: uuid(),
+        thisMonth: false,
+      });
+    }
+  }
+  return daysArray;
+}
+
+export default getMonthDetails;
