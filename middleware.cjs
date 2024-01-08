67bfec92fbeb5a8c7259bd202836eaf95dd9f602
Ono Keita <oh.kitbsk.1028@icloud.com>
2024-01-08 11:51:04 +0000

    fix: json server関連ファイルの修正

--- middleware.cjs
+++ middleware.cjs
@@ -2,8 +2,8 @@ module.exports = function (req, res, next) {
   if (req.method === 'POST') {
     req.method = 'GET';
 
-    if (req.url === '/api/v1-payment-registration') {
-      req.url = '/v1-payment-registration';
+    if (req.url === '/api/v1-payment-put') {
+      req.url = '/v1-payment-put';
     }
 
     if (req.url === '/api/v1-payment-get-all') {
