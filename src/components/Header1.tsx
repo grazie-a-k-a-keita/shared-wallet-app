1fac31da03902d0ef4e40deb39e42661460b2631
Ono Keita <oh.kitbsk.1028@icloud.com>
2024-01-10 12:03:12 +0000

    feat: Iphone以外の場合に画面上部に余白を追加

--- src/components/Header1.tsx
+++ src/components/Header1.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import classes from './Header1.module.scss';
 import MuiButton from './MUIcomponents/MuiButton';
 import ToggleButton from './MUIcomponents/MuiToggleButton';
@@ -7,8 +9,27 @@ import type { Header1Props } from '../types/props';
 function Header1(props: Header1Props) {
   const { leftButtonName, rightButtonName, setToggleStatus, amount, onClick } = props;
 
+  const [headerStyle, setHeaderStyle] = useState({});
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isIPhone = /iPhone/i.test(navigator.userAgent);
+
+      if (!isIPhone) {
+        setHeaderStyle({ paddingTop: '1rem' });
+      } else {
+        setHeaderStyle({});
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
+      <div style={headerStyle} />
       <div className={classes.toggleButtonContainer}>
         <div className={classes.toggleButtonBackground}>
           <ToggleButton
