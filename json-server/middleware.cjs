f53e9856a60482893e562eb6371e5d33d4ce348d
Ono Keita <oh.kitbsk.1028@icloud.com>
2024-03-25 22:58:15 +0000

    docs: モック関連整理

--- json-server/middleware.cjs
+++ json-server/middleware.cjs
@@ -1,20 +1,20 @@
 module.exports = function (req, res, next) {
-  if (req.method === 'POST') {
+  if (req.method !== 'GET') {
     req.method = 'GET';
 
-    if (req.url === '/api/v1-payment-put') {
+    if (req.url === '/v1-payment-put') {
       req.url = '/v1-payment-put';
     }
 
-    if (req.url === '/api/v1-payment-get-all') {
+    if (req.url === '/v1-payment-get-all') {
       req.url = '/v1-payment-get-all';
     }
 
-    if (req.url === '/api/v1-payment-get-detail') {
+    if (req.url === '/v1-payment-get-detail') {
       req.url = '/v1-payment-get-detail';
     }
 
-    if (req.url === '/api/v1-payment-post-item') {
+    if (req.url === '/v1-payment-post-item') {
       req.url = '/v1-payment-post-item';
     }
   }
